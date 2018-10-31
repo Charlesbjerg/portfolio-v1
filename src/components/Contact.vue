08bf3867b2487b7701085b5ff27fafbfe3d31fe8
Charlie Underhill <charlie45@live.co.uk>
2018-10-31 21:11:24 +0000

    Some changes to contact, main and PortfolioItem

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,19 @@
 <template>
-    <h1>Get in touch</h1>
+    <section>
+        <h1>Get in touch</h1>
+        <form>
+            <div class="field">
+                <label class="label" for="name">Full Name</label>
+                <input class="input" type="text" id="text" v-bind="name"/>
+            </div>  
+        </form>
+    </section>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-
 </style>
 
